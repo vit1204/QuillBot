4c4338dccf7d7cf0bd36693992285a4d09c054f0
vit1204 <nguyenthanhviet12042004@gmail.com>
2025-04-21 22:32:59 +0700

    fix some buggs

--- src/api/openai.ts
+++ src/api/openai.ts
@@ -2,6 +2,7 @@ import { GoogleGenerativeAI } from '@google/generative-ai'
 
 const API_KEY = import.meta.env.VITE_APP_OPENAI_API_KEY
 
+
 const genAI = new GoogleGenerativeAI(API_KEY)
 
 const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
